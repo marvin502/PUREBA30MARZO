5750a40bdb896c8ae5c1313e93eadaa473ee5e2c
pc <pc@DESKTOP-S3AILKD.home>
2020-04-02 14:00:55 -0600

    Segundo MODIFICAION

--- EJERCICIO/src/EJERCICIO.java
+++ EJERCICIO/src/EJERCICIO.java
@@ -15,6 +15,12 @@ public class EJERCICIO {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        
+        System.out.println("Nombre: Luis Humberto Ruiz Castillo");
+        System.out.println("Direccion: 8va Calle A 11-30 zona 1 interior");
+        System.out.println("Telefono: 37070944");
+        
     }
     
 }
